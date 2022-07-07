c55d442565c36efac42de9ee66aec16ce4cb306b
kateincoding <kateincoding@gmail.com>
2022-07-07 11:44:42 -0500

    [configuration]done

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
 // new headers
+import '@aws-amplify/ui-vue'
 import Amplify from 'aws-amplify';
-import '@aws-amplify/ui-vue';
 import aws_exports from './aws-exports';
+import router from './router'
 
 Amplify.configure(aws_exports);
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
