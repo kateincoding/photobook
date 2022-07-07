9bf46ba080399766031cf1a3574d1487f8a84af7
kateincoding <kateincoding@gmail.com>
2022-07-07 11:32:26 -0500

    [configuration with amplify]

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
+// new headers
+import Amplify from 'aws-amplify';
+import '@aws-amplify/ui-vue';
+import aws_exports from './aws-exports';
 
+Amplify.configure(aws_exports);
 Vue.config.productionTip = false
 
 new Vue({
